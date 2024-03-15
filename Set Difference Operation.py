6ccccc1c6e9cd25119f7c3a80b772121d4ac10e3
AYA <118719938+Amina-moon@users.noreply.github.com>
2024-03-15 06:49:02 +0300

    Create Set Difference Operation.py

---
+++ Set Difference Operation.py
@@ -0,0 +1,6 @@
+english_subscriber=int(input())
+english_subscriber_list=set(map(int,input().split()))
+french_subscriber=int(input())
+french_subscriber_list=set(map(int,input().split()))
+only_english_subscriber=english_subscriber_list.difference(french_subscriber_list)
+print(len(only_english_subscriber))
